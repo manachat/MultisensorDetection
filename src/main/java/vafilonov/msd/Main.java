936ea2852cc2fcae6a93b23d91c07e1db0391c75
manachat <vafilonov@edu.hse.ru>
2021-02-25 22:19:02 +0300

    i started to move

--- src/main/java/vafilonov/msd/Main.java
+++ src/main/java/vafilonov/msd/Main.java
@@ -7,19 +7,45 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import vafilonov.msd.controllers.MainSceneController;
 
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main extends Application {
 
+    public static Scene scene;
+
     public static void main(String[] args) {
+        Properties prop = new Properties();
+        final String file = "app.config";
+        try {
+            prop.load(new FileInputStream(file));
+        } catch (IOException ioex) {
+            System.err.println("Couldn't load configuration file.");
+            System.exit(-1);
+        }
+
+        System.out.println(prop.getProperty("app.workdir"));
+
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        MainSceneController controller = new MainSceneController();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainScene.fxml"));
-        Scene scene = new Scene(root, 600, 400);
 
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/MainScene.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, screenSize.getWidth(), screenSize.getHeight());
+        Main.scene = scene;
         stage.setScene(scene);
+
+        ((MainSceneController) loader.getController()).postInitialize();
+
         stage.show();
      }
 }
--- src/main/java/vafilonov/msd/controllers/MainSceneController.java
+++ src/main/java/vafilonov/msd/controllers/MainSceneController.java
@@ -2,149 +2,98 @@ package vafilonov.msd.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import org.gdal.gdal.Dataset;
-import org.gdal.gdal.gdal;
-import org.gdal.gdalconst.gdalconstConstants;
-import org.gdal.osr.CoordinateTransformation;
-import org.gdal.osr.SpatialReference;
+import org.gdal.gdal.*;
+import vafilonov.msd.Main;
 
-import java.util.Arrays;
+import java.io.FileInputStream;
+import java.util.Vector;
 
 
 public class MainSceneController {
 
 
-
-
     @FXML
-    private Label label;
+    private ImageView view;
+
 
     @FXML
     private Button button;
 
     @FXML
-    private TextArea area;
+    public void initialize() {
+
+    }
+
+    public void postInitialize() {
+
+        Main.scene.widthProperty().addListener(value -> {
+            view.setFitWidth(Main.scene.getWidth() - button.getWidth());
+        });
+        Main.scene.heightProperty().addListener(value -> {
+            view.setFitHeight(Main.scene.getHeight());
+        });
+
+    }
+
 
     @FXML
-    void buttonClickHandler(MouseEvent e) {
-        gdal.AllRegister();
-        Dataset set1 = gdal.Open("/home/vfilonov/programming/geodata/clipped_2020_MSK.tif", gdalconstConstants.GA_ReadOnly);
-        Dataset set2 = gdal.Open("/home/vfilonov/programming/geodata/clipped_2017_MSK.tif", gdalconstConstants.GA_ReadOnly);
-        if (set1 == null || set2 == null) {
-            label.setText("You are such a failure");
-        } else {
-            area.appendText(set1.getRasterXSize() + " " + set1.getRasterYSize() + '\n' +
-                    set1.GetDriver().getShortName() + set1.GetRasterCount() + '\n');
-            double[] props = set1.GetGeoTransform();
-            if (props != null) {
-                area.appendText(Arrays.toString(props) + '\n');
-            }
-            var meta = set1.GetMetadata_List("");
-            for (var el : meta) {
-                area.appendText(el.toString() + " ");
-            }
+    void buttonClickHandler(MouseEvent e) throws Exception {
+        Dataset data = null;
+        Band band = null;
 
-            area.appendText("\n\n");
 
-            area.appendText(set2.getRasterXSize() + " " + set2.getRasterYSize() + '\n' +
-                    set2.GetDriver().getShortName() + set2.GetProjection() + '\n');
-            area.appendText(Arrays.toString(set1.GetGeoTransform()) + '\n');
 
-            var meta2 = set2.GetMetadata_List("");
-            for (var el : meta2) {
-                area.appendText(el.toString() + " ");
-            }
+        try {
 
-            GDALInfoReportCorner(set1, "Upper Left ", 0.0, 0.0);
-            GDALInfoReportCorner(set1, "Lower Left ", 0.0, set1.getRasterYSize());
-            GDALInfoReportCorner(set1, "Upper Right", set1.getRasterXSize(), 0.0);
-            GDALInfoReportCorner(set1, "Lower Right", set1.getRasterXSize(), set1.getRasterYSize());
-            System.out.println("\n");
-            GDALInfoReportCorner(set2, "Upper Left ", 0.0, 0.0);
-            GDALInfoReportCorner(set2, "Lower Left ", 0.0, set1.getRasterYSize());
-            GDALInfoReportCorner(set2, "Upper Right", set2.getRasterXSize(), 0.0);
-            GDALInfoReportCorner(set2, "Lower Right", set2.getRasterXSize(), set2.getRasterYSize());
-            set1.delete();
-            set2.delete();
         }
+
+        Image im = new Image(new FileInputStream("/home/vfilonov/Pictures/inn.png"));
+        view.setFitHeight(im.getRequestedHeight());
+        view.setFitWidth(im.getRequestedWidth());
+        view.setImage(im);
+
+
 
     }
 
-    static boolean GDALInfoReportCorner(Dataset hDataset, String corner_name,
-                                        double x, double y)
 
-    {
-        double dfGeoX, dfGeoY;
-        String pszProjection;
-        double[] adfGeoTransform = new double[6];
-        CoordinateTransformation hTransform = null;
 
-        System.out.print(corner_name + " ");
 
-        /* -------------------------------------------------------------------- */
-        /*      Transform the point into georeferenced coordinates.             */
-        /* -------------------------------------------------------------------- */
-        hDataset.GetGeoTransform(adfGeoTransform);
-        {
-            pszProjection = hDataset.GetProjectionRef();
+    private void clipRasterByCoords() {
+        // -projwin 37.140077458 55.991533828 37.814126275 55.73792501 -of GTiff
+        TranslateOptions options = null;
+        Dataset source = null;
 
-            dfGeoX = adfGeoTransform[0] + adfGeoTransform[1] * x
-                    + adfGeoTransform[2] * y;
-            dfGeoY = adfGeoTransform[3] + adfGeoTransform[4] * x
-                    + adfGeoTransform[5] * y;
-        }
+        Vector ops = new Vector();
+        ops.add("-projwin");
+        ops.add("37.140077458");
+        ops.add("55.991533828");
+        ops.add("37.814126275");
+        ops.add("55.73792501");
+        try {
+            options = new TranslateOptions(ops);
+            gdal.AllRegister();
+            source = gdal.Open("/home/vfilonov/programming/geodata/MSK_2017_vv.tif");
 
-        if (adfGeoTransform[0] == 0 && adfGeoTransform[1] == 0
-                && adfGeoTransform[2] == 0 && adfGeoTransform[3] == 0
-                && adfGeoTransform[4] == 0 && adfGeoTransform[5] == 0) {
-            System.out.println("(" + x + "," + y + ")");
-            return false;
-        }
+            gdal.Translate("/home/vfilonov/programming/geodata/translated.tif", source, options);
 
-        /* -------------------------------------------------------------------- */
-        /*      Report the georeferenced coordinates.                           */
-        /* -------------------------------------------------------------------- */
-        System.out.print("(" + dfGeoX + "," + dfGeoY + ") ");
 
-        /* -------------------------------------------------------------------- */
-        /*      Setup transformation to lat/long.                               */
-        /* -------------------------------------------------------------------- */
-        if (pszProjection != null && pszProjection.length() > 0) {
-            SpatialReference hProj, hLatLong = null;
-
-            hProj = new SpatialReference(pszProjection);
-            if (hProj != null)
-                hLatLong = hProj.CloneGeogCS();
-
-            if (hLatLong != null) {
-                /* New in GDAL 1.10. Before was "new CoordinateTransformation(srs,dst)". */
-                hTransform = CoordinateTransformation.CreateCoordinateTransformation(hProj, hLatLong);
+        } catch (RuntimeException rex) {
+            System.out.println("Runtime caught");
+            rex.printStackTrace();
+        } finally {
+            if (source != null) {
+                source.delete();
             }
-
-            if (hProj != null)
-                hProj.delete();
+            if (options != null) {
+                options.delete();
+            }
         }
-
-        /* -------------------------------------------------------------------- */
-        /*      Transform to latlong and report.                                */
-        /* -------------------------------------------------------------------- */
-        if (hTransform != null) {
-            double[] transPoint = new double[3];
-            hTransform.TransformPoint(transPoint, dfGeoX, dfGeoY, 0);
-            System.out.print("(" + gdal.DecToDMS(transPoint[0], "Long", 2));
-            System.out
-                    .print("," + gdal.DecToDMS(transPoint[1], "Lat", 2) + ")");
-        }
-
-        if (hTransform != null)
-            hTransform.delete();
-
-        System.out.println("");
-
-        return true;
     }
+
+
 
 }
