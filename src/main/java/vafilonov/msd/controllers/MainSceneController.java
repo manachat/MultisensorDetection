49c7ed04292b9a362f0a924ae8f6b2f6123c42c9
manachat <vafilonov@edu.hse.ru>
2021-02-26 16:29:09 +0300

    image render mvp

--- src/main/java/vafilonov/msd/controllers/MainSceneController.java
+++ src/main/java/vafilonov/msd/controllers/MainSceneController.java
@@ -1,14 +1,21 @@
 package vafilonov.msd.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.*;
 import javafx.scene.input.MouseEvent;
 import org.gdal.gdal.*;
+import org.gdal.gdalconst.gdalconst;
+import org.gdal.gdalconst.gdalconstConstants;
 import vafilonov.msd.Main;
 
 import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.nio.ShortBuffer;
 import java.util.Vector;
 
 
@@ -41,19 +48,82 @@ public class MainSceneController {
 
     @FXML
     void buttonClickHandler(MouseEvent e) throws Exception {
+        gdal.AllRegister();
         Dataset data = null;
         Band band = null;
+        int x,y;
 
+        String path = "/home/vfilonov/programming/geodata/MSK_2017_vv.tif";
+
+        WritableImage img = null;
+        int[][] raw = null;
+        double[] stats = new double[2];
 
 
         try {
+            data = gdal.Open(path, gdalconstConstants.GA_ReadOnly);
 
+
+            band = data.GetRasterBand(1);
+
+            band.ComputeBandStats(stats);
+
+            double min = stats[0] - 2*stats[1];
+            double max = stats[0] + 2*stats[1];
+
+            min = Math.max(0.0, min);
+
+            x = band.GetXSize();
+            y = band.getYSize();
+
+            raw = new int[y][x];
+            short[] intermediate = new short[x];
+
+
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(2*x);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            for (int j = 0; j < y; j++) {
+                band.ReadRaster_Direct(0, j, x, 1, x, 1, gdalconst.GDT_Int16, byteBuffer);
+                byteBuffer.asShortBuffer().get(intermediate);
+                for (int i = 0; i < x; i++) {
+                    int normalized = (int) ((intermediate[i] - min) * 255 / (max - min));
+
+                    normalized = Math.max(0, normalized);
+                    normalized = Math.min(255, normalized);
+
+                    raw[j][i] = 255 << 24 | normalized << 16 | normalized << 8 | normalized;
+                }
+
+            }
+
+
+
+        } catch (Exception ex) {
+
+            System.err.println("failed to open");
+            return;
+        } finally {
+            if (data != null)
+                data.delete();
         }
 
-        Image im = new Image(new FileInputStream("/home/vfilonov/Pictures/inn.png"));
-        view.setFitHeight(im.getRequestedHeight());
-        view.setFitWidth(im.getRequestedWidth());
-        view.setImage(im);
+        img = new WritableImage(x, y);
+
+        PixelWriter writer = img.getPixelWriter();
+        PixelFormat<IntBuffer> format =  PixelFormat.getIntArgbInstance();
+
+        for (int j = 0; j < y; j++) {
+            writer.setPixels(0, j, x, 1, format, raw[j], 0, x);
+        }
+
+
+        //Image im = new Image(new FileInputStream("/home/vfilonov/Pictures/inn.png"));
+        //view.setFitHeight(img.getRequestedHeight());
+        //view.setFitWidth(img.getRequestedWidth());
+
+        view.setViewport(new Rectangle2D(x / 2, y / 2, x /2, y/2));
+        view.setImage(img);
 
 
 
@@ -92,6 +162,32 @@ public class MainSceneController {
                 options.delete();
             }
         }
+    }
+
+
+    @FXML
+    private void saveMenuHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void saveAsMenuHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void infoMenuHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void loadMenuHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void aboutMenuHandler(ActionEvent event) {
+
     }
 
 
